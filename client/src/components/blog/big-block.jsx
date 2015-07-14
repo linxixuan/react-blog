89f5ed4b3522e5c4ca9a295d98c1317ec66f5d25
linxixuan <zmx.morgan@gmail.com>
2015-07-14 22:37:09 +0800

    add blog block

--- client/src/components/blog/big-block.jsx
+++ client/src/components/blog/big-block.jsx
@@ -1,10 +1,32 @@
 var React = require('react');
 
 module.exports = React.createClass({
+	getInitialState: function () {
+		// 伪造数据
+		return {
+			title: '博客名',
+			link: 'http://baidu.com',
+			summary: '简介简介简介简介简介简介简介简介简介简介简介简介简介简介',
+			content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容;,
+			tags: ['测试', '测试试试', 'sss'],
+			date: new Date(),
+		};
+	},
+
 	render: function () {
 		return (
-			<div>
-				<h2>一个blog模块</h2>
+			<div className="blog-block">
+				<h2 className="blog-block__title"><a href="{this.state.href}" target="_blank">{this.state.title}</a></h2>
+				<div className="blog-block__content">
+					<a href="{this.state.href}" target="_blank">{this.state.content}</a>
+				</div>
+				<div className="blog-block__extra">
+				`	this.state.tags.map(function (item) {
+						return (
+							<a href="http://baidu.com?tags=" + {item} target="_blank">{item}</a>
+						);
+					})
+				</div>
 			</div>
 		);
 	}
