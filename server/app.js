005efb2ce5db29bed4fa5525fc754680f4ebe90a
linxixuan <zmx.morgan@gmail.com>
2015-08-11 20:47:13 +0800

    change router as module

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@
  */
 var koa = require('koa');
 var static = require('koa-static');
-var router = require('koa-router')();
 var mongoose = require('mongoose');
 
 var blogController = require('./controllers/blog.js');
@@ -11,12 +10,10 @@ var blogController = require('./controllers/blog.js');
 mongoose.connect('mongodb://localhost/zmx');
 
 var app = koa();
-// static file
 app.use(static('.'));
 
-router.get('/', require('./controllers/index.js'));
-router.get('/blogs', require('./controllers/blog.js'));
- 
+var router = require('./router.js');
+
 app
   .use(router.routes())
   .use(router.allowedMethods());
